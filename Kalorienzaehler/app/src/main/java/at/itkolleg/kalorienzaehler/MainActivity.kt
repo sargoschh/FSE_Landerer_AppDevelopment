b315df9171d5d281f772743873f13f49e1ce31f7
sargoschh <sargosch@tsn.at>
2023-06-17 20:31:43 +0200

    Kalorienzähler eingerichtet

---
+++ Kalorienzaehler/app/src/main/java/at/itkolleg/kalorienzaehler/MainActivity.kt
@@ -0,0 +1,11 @@
+package at.itkolleg.kalorienzaehler
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
