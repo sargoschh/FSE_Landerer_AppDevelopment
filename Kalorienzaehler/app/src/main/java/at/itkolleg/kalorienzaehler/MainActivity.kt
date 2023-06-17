98d4861d54329492b0bffbe38bd88c813aa61be2
sargoschh <sargosch@tsn.at>
2023-06-17 23:14:47 +0200

    Layout abgeschlossen

--- Kalorienzaehler/app/src/main/java/at/itkolleg/kalorienzaehler/MainActivity.kt
+++ Kalorienzaehler/app/src/main/java/at/itkolleg/kalorienzaehler/MainActivity.kt
@@ -2,10 +2,16 @@ package at.itkolleg.kalorienzaehler
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TableLayout
+import android.widget.TableRow
+import android.widget.TextView
+import at.itkolleg.todolist.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
