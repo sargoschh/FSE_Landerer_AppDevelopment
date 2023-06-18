fe14a07a501af526737e3c5e75abfa57c9844ef1
sargoschh <sargosch@tsn.at>
2023-06-18 16:03:17 +0200

    So geht es - mit Fehlern

--- Kalorienzaehler/app/src/main/java/at/itkolleg/kalorienzaehler/MealAdapter.kt
+++ Kalorienzaehler/app/src/main/java/at/itkolleg/kalorienzaehler/MealAdapter.kt
@@ -3,16 +3,19 @@ package at.itkolleg.kalorienzaehler
 import android.graphics.Paint
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.CheckBox
 import android.widget.TableLayout
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import at.itkolleg.kalorienzaehler.databinding.ItemMealsBinding
-import at.itkolleg.kalorienzaehler.databinding.ItemTableBinding
+import at.itkolleg.kalorienzaehler.databinding.ItemTablesBinding
 
 class MealAdapter(private val meals: MutableList<Meal>, private var daily: Daily, private val tblDailys: TableLayout) : RecyclerView.Adapter<MealAdapter.MealViewHolder>() {
 
     class MealViewHolder(val binding: ItemMealsBinding, val tableLayout: TableLayout) : RecyclerView.ViewHolder(binding.root) {
+        val tvMealTitle: TextView = binding.tvMealTitle
+        val cbDone: CheckBox = binding.cbDone
         val tvTblKcal: TextView = tableLayout.findViewById(R.id.tvTblKcal)
         val tvTblFat: TextView = tableLayout.findViewById(R.id.tvTblFat)
         val tvTblProtein: TextView = tableLayout.findViewById(R.id.tvTblPro)
@@ -22,7 +25,7 @@ class MealAdapter(private val meals: MutableList<Meal>, private var daily: Daily
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MealViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val binding = ItemMealsBinding.inflate(inflater, parent, false)
-        val tableLayoutBinding = ItemTableBinding.inflate(inflater, parent, false)
+        val tableLayoutBinding = ItemTablesBinding.inflate(inflater, parent, false)
         val clItem = tableLayoutBinding.root as ConstraintLayout
 
         val tableLayout = clItem.findViewById<TableLayout>(R.id.tblDailys)
@@ -31,6 +34,30 @@ class MealAdapter(private val meals: MutableList<Meal>, private var daily: Daily
 
         return MealViewHolder(binding, tableLayout)
     }
+
+    override fun onBindViewHolder(holder: MealViewHolder, position: Int) {
+        val currentMeal = meals[position]
+
+        val mealDetails = holder.itemView.context.getString(
+            R.string.meal_details,
+            currentMeal.title,
+            currentMeal.kcal,
+            currentMeal.fat,
+            currentMeal.protein,
+            currentMeal.sugar
+        )
+
+        holder.binding.tvMealTitle.text = mealDetails
+        holder.binding.cbDone.isChecked = currentMeal.isChecked
+
+        holder.binding.cbDone.setOnCheckedChangeListener { _, isChecked ->
+            toggleStrikeThrough(holder.binding.tvMealTitle, isChecked)
+            currentMeal.isChecked = isChecked
+            updateTable()
+        }
+    }
+
+    override fun getItemCount() = meals.size
 
     fun addMeal(meal: Meal) {
         meals.add(meal)
@@ -60,7 +87,6 @@ class MealAdapter(private val meals: MutableList<Meal>, private var daily: Daily
     }
 
     private fun updateTable() {
-        // Aktualisiere die Werte in der Tabelle
         tblDailys.findViewById<TextView>(R.id.tvTblKcal).text = "%2d".format(daily.kcal)
         tblDailys.findViewById<TextView>(R.id.tvTblFat).text = "%.2f".format(daily.fat)
         tblDailys.findViewById<TextView>(R.id.tvTblPro).text = "%.2f".format(daily.protein)
@@ -74,32 +100,4 @@ class MealAdapter(private val meals: MutableList<Meal>, private var daily: Daily
             tvMeal.paintFlags = tvMeal.paintFlags and Paint.STRIKE_THRU_TEXT_FLAG.inv()
         }
     }
-
-    override fun onBindViewHolder(holder: MealViewHolder, position: Int) {
-        val currentMeal = meals[position]
-
-        val mealDetails = holder.itemView.context.getString(
-            R.string.meal_details,
-            currentMeal.title,
-            currentMeal.kcal,
-            currentMeal.fat,
-            currentMeal.protein,
-            currentMeal.sugar
-        )
-
-        holder.binding.tvMealTitle.text = mealDetails
-        holder.binding.cbDone.isChecked = currentMeal.isChecked
-
-        holder.tvTblKcal.text = "Kcal: %2d".format(currentMeal.kcal)
-        holder.tvTblFat.text = "Fat: %.2f".format(currentMeal.fat)
-        holder.tvTblProtein.text = "Protein: %.2f".format(currentMeal.protein)
-        holder.tvTblSugar.text = "Sugar: %.2f".format(currentMeal.sugar)
-
-        holder.binding.cbDone.setOnCheckedChangeListener { _, isChecked ->
-            toggleStrikeThrough(holder.binding.tvMealTitle, isChecked)
-            currentMeal.isChecked = isChecked
-        }
-    }
-
-    override fun getItemCount() = meals.size
 }
